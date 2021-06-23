3ab9fd86e289da8fbcadb4400e705d604786eabf
Ľubor Jurena <lubor@jurena.sk>
2021-06-23 16:33:49 +0200

    minor changes

--- debian_buster/run.sh
+++ debian_buster/run.sh
@@ -1,18 +1,14 @@
 #/bin/bash
 if which docker >/dev/null; then
-    docker image rm nginx-build-buster
-    docker build -t nginx-build-buster .
-    docker run --name nginx-build-buster -it -d nginx-build-buster
-    docker exec nginx-build-buster bash -c "mkdir /deb ; mv /usr/src/nginx_*.deb /deb"
-    docker cp nginx-build-buster:/deb .
-    sleep 2
-    docker rm -f nginx-build-buster
+    CONTAINERS=docker
 else
-    podman image rm nginx-build-buster
-    podman build -t nginx-build-buster .
-    podman run --name nginx-build-buster -it -d nginx-build-buster
-    podman exec nginx-build-buster bash -c "mkdir /deb ; mv /usr/src/nginx_*.deb /deb"
-    podman cp nginx-build-buster:/deb .
-    sleep 2
-    podman rm -f nginx-build-buster
+    CONTAINERS=podman
 fi
+
+$CONTAINERS image rm nginx-build-buster
+$CONTAINERS build -t nginx-build-buster .
+$CONTAINERS run --name nginx-build-buster -it -d nginx-build-buster
+$CONTAINERS exec nginx-build-buster bash -c "mkdir /deb ; mv /usr/src/nginx_*.deb /deb"
+$CONTAINERS cp nginx-build-buster:/deb .
+sleep 2
+$CONTAINERS
